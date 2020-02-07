9f33d052878cae4f5c3969deae349659f3ac5f36
guo-quan <488776517@qq.com>
2020-02-07 17:43:16 +0800

    pref：更换种子工程

--- dawn-community-manage-service/src/main/java/com/gsafety/dawn/community/manage/service/entity/metadata/EnumValueContract.java
+++
@@ -1,9 +0,0 @@
-package com.gsafety.dawn.community.manage.service.entity.metadata;
-
-/**
- * Created by numsg on 2016/6/12.
- * 枚举继承接口约定
- */
-public interface EnumValueContract<T> {
-   int getParamType();
-}

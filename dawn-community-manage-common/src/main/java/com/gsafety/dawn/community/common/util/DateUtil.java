4ad71273c60fa9c4c316312165b3eb2b4acd33df
liujian <984035589@qq.com>
2020-02-09 10:12:13 +0800

    fix： 定时请求数据并保存

--- dawn-community-manage-common/src/main/java/com/gsafety/dawn/community/common/util/DateUtil.java
+++ dawn-community-manage-common/src/main/java/com/gsafety/dawn/community/common/util/DateUtil.java
@@ -55,4 +55,24 @@ public class DateUtil {
         return   Timestamp.valueOf(format);
     }
 
+
+    public static Date getStartTimeDate() {
+        Calendar todayStart = Calendar.getInstance();
+        todayStart.set(Calendar.HOUR, 0);
+        todayStart.set(Calendar.MINUTE, 0);
+        todayStart.set(Calendar.SECOND, 0);
+        todayStart.set(Calendar.MILLISECOND, 0);
+        return todayStart.getTime();
+    }
+
+    public static Date getEndTimeDate() {
+        Calendar todayEnd = Calendar.getInstance();
+        todayEnd.set(Calendar.HOUR, 23);
+        todayEnd.set(Calendar.MINUTE, 59);
+        todayEnd.set(Calendar.SECOND, 59);
+        todayEnd.set(Calendar.MILLISECOND, 999);
+        return todayEnd.getTime();
+    }
+
+
 }

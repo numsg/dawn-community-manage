3f847bf7c524cc1cf006d043154bcd1c1792d424
qianqi <qianqi@gsafety.com>
2020-02-13 21:50:49 +0800

    fix: 修改多租户查询小区楼栋单元已排查未排查统计中发热数量错误问题

--- dawn-community-manage-service/src/main/java/com/gsafety/dawn/community/manage/service/repository/refactor/TroubleshootRecordRepository.java
+++ dawn-community-manage-service/src/main/java/com/gsafety/dawn/community/manage/service/repository/refactor/TroubleshootRecordRepository.java
@@ -23,9 +23,10 @@ public interface TroubleshootRecordRepository extends JpaRepository<Troubleshoot
     @Query("select c from TroubleshootRecordEntity c where c.personBaseId  = :personBaseId")
     TroubleshootRecordEntity findByPersonBaseId(@Param("personBaseId") String personBaseId);
 
-    @Query("select new com.gsafety.dawn.community.manage.service.entity.refactor.PlotBuildingUnitStaffEntity(count(c),c.building,c.unitNumber,c.createDate,c.plot,c.isExceedTemp) " +
+    @Query("select new com.gsafety.dawn.community.manage.service.entity.refactor.PlotBuildingUnitStaffEntity(count(c),c.building,c.unitNumber,c.createDate,c.plot," +
+            "SUM(CASE WHEN isExceedTemp IS TRUE THEN 1 ELSE 0 END)) " +
             "from TroubleshootRecordEntity c where c.multiTenancy  = :multiTenancy " +
-            "group by c.building,c.unitNumber,c.createDate,c.plot,c.isExceedTemp")
+            "group by c.building,c.unitNumber,c.createDate,c.plot")
     List<PlotBuildingUnitStaffEntity> findPlotBuildingUnitStaff(@Param("multiTenancy") String multiTenancy);
 
     @Query("select new com.gsafety.dawn.community.manage.service.entity.refactor.PlotReportingStaffEntity(count(c),c.plot) " +
